c3a2f5523936979b9863d3fd3ad1d7af89ee778a
Omulosi <mulongojohnpaul@gmail.com>
2019-10-13 19:18:14 +0300

    Add a form modal for adding new smurfs. No functionality yet

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import SmurfsTable from './SmurfsTable';
 import Header from './Header';
+import FormModal from './FormModal';
+import Modal from './Modal';
 
 import "./App.css";
 
 
 const  App = props => {
+  const [state, setState] = useState({showModal: false });
+
+  const toggleModal = () => setState({showModal: !state.showModal});
+
   return (
     <div className="App">
       <Header />
       <div className="main-content container">
-        <button className="add-btn">Add a Smurf</button>
+        <button className="add-btn" onClick={toggleModal}>Add a Smurf</button>
         <SmurfsTable />
+
+        {
+          state.showModal? (
+            <Modal toggleModal={toggleModal}>
+              <FormModal />
+            </Modal>
+          ): null
+        }
       </div>
     </div>
   );
