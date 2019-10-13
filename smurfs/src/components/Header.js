b1d49e1c2cb12ed7a358a331d86fcd07cf3e0e26
Omulosi <mulongojohnpaul@gmail.com>
2019-10-13 17:15:48 +0300

    Initial setup
    Create Header and SmurfTable components with dummy placeholder data
    for initialdisplay and add styles

---
+++ smurfs/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="container-fluid shadow p-2 mb-5 bg-white">
+      <header className="header">
+        <h1>Smurfs</h1>
+      </header>
+    </div>
+  )
+}
+
+export default Header;
